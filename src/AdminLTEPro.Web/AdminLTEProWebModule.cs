524b220cdf7723a6cbcadd0a087e25d51ff99065
Wagner Xu <wagnerhsu@hotmail.com>
2021-01-27 17:46:21 +0800

    Customize Theme

--- src/AdminLTEPro.Web/AdminLTEProWebModule.cs
+++ src/AdminLTEPro.Web/AdminLTEProWebModule.cs
@@ -85,8 +85,13 @@ namespace AdminLTEPro.Web
             ConfigureNavigationServices();
             ConfigureAutoApiControllers();
             ConfigureSwaggerServices(context.Services);
+            ConfigureChromeIssue(context);
         }
 
+        private void ConfigureChromeIssue(ServiceConfigurationContext context)
+        {
+            context.Services.AddSameSiteCookiePolicy();
+        }
         private void ConfigureUrls(IConfiguration configuration)
         {
             Configure<AppUrlOptions>(options =>
@@ -152,18 +157,8 @@ namespace AdminLTEPro.Web
         {
             Configure<AbpLocalizationOptions>(options =>
             {
-                options.Languages.Add(new LanguageInfo("ar", "ar", "العربية"));
-                options.Languages.Add(new LanguageInfo("cs", "cs", "Čeština"));
                 options.Languages.Add(new LanguageInfo("en", "en", "English"));
-                options.Languages.Add(new LanguageInfo("hu", "hu", "Magyar"));
-                options.Languages.Add(new LanguageInfo("fr", "fr", "Français"));
-                options.Languages.Add(new LanguageInfo("pt-BR", "pt-BR", "Português"));
-                options.Languages.Add(new LanguageInfo("ru", "ru", "Русский"));
-                options.Languages.Add(new LanguageInfo("tr", "tr", "Türkçe"));
                 options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "简体中文"));
-                options.Languages.Add(new LanguageInfo("zh-Hant", "zh-Hant", "繁體中文"));
-                options.Languages.Add(new LanguageInfo("de-DE", "de-DE", "Deutsch", "de"));
-                options.Languages.Add(new LanguageInfo("es", "es", "Español"));
             });
         }
 
@@ -212,6 +207,7 @@ namespace AdminLTEPro.Web
                 app.UseErrorPage();
             }
 
+            app.UseCookiePolicy();
             app.UseCorrelationId();
             app.UseVirtualFiles();
             app.UseRouting();
---
+++ src/AdminLTEPro.Web/Pages/AdminLTEProPage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+using Volo.Abp.Localization;
+
+namespace AdminLTEPro.Web.Pages
+{
+    public abstract class AdminLTEProPage: AbpPage
+    {
+        [RazorInject]
+        public ILanguageProvider LanguageProvider { get; set; }
+    }
+}
